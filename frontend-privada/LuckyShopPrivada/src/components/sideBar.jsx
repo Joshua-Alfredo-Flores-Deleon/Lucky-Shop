88a9a2a8076841232842fb5c72eb9852a9624dc2
Joshua-Alfredo-Flores-Deleon <20240228@ricaldone.edu.sv>
2026-06-29 14:41:08 -0600

    Se agrego el apartado de sidebar en la privada

--- frontend-privada/LuckyShopPrivada/src/components/sideBar.jsx
+++ frontend-privada/LuckyShopPrivada/src/components/sideBar.jsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../productos.css'
-// 1. Importas la imagen dándole un nombre (por ejemplo: logosite)
+
+// Importación de imágenes y recursos
 import logosite from '../assets/image-removebg-preview.png' 
 import HomeIcon from '../assets/icons8-casa-24.png'
 import ventaIcon from '../assets/ventas.png'
@@ -13,73 +14,109 @@ import FinanzasIcon from '../assets/finanza.png'
 import notificacionIcon from '../assets/icons8-recordatorios-de-citas-48.png'
 import PerfilIcon from '../assets/cuales-son-las-razas-de-gatos-mas-populares-en-colombia.jpg'
 
-const sideBar = () => {
+const SideBar = () => {
+  const location = useLocation()
+  // Función para determinar si una ruta está activa
+  const isActive = (path) => {
+    return location.pathname === path ? 'active' : ''
+  }
   return (
-    <nav className="contenedor">
-      <div className="sidebar">
-        {/* Logo */}
-        <Link to="/home" className="">
-          {/* 2. Usas la variable entre llaves en el src */}
-          <img className='logo' src={logosite} alt="Logo de Lucky Shop" />
-            <hr className='line'/>
-        </Link>
-
-        {/* Links */}
-        <ul className="">
-          <li className='apartado'>
-            <img src={HomeIcon} alt="Icono de Inicio" />
-            <Link to="/home" className="text">Inicio</Link>
+    <nav className="luckyshop-nav-container">
+      {/* Sidebar Principal */}
+      <aside className="luckyshop-sidebar">
+        {/* Contenedor del Logo */}
+        <div className="logo-container">
+          <Link to="/home" className="logo-link">
+            <img className="logo-img" src={logosite} alt="Logo de Lucky Shop" />
+          </Link>
+        </div>
+        {/* Separador Estilizado */}
+        <div className="sidebar-divider"></div>
+        {/* Lista de Navegación */}
+        <ul className="sidebar-menu">
+          <li className={`menu-item ${isActive('/home')}`}>
+            <Link to="/home" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={HomeIcon} alt="Inicio" />
+              </div>
+              <span className="menu-text">Inicio</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={ventaIcon} alt=" Icono de Ventas" />
-            <Link to="/ventas" className="text">Ventas</Link>
+          <li className={`menu-item ${isActive('/ventas')}`}>
+            <Link to="/ventas" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={ventaIcon} alt="Ventas" />
+              </div>
+              <span className="menu-text">Ventas</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={productoIcon} alt="Icono de productos" />
-            <Link to="/productos" className="text">Productos</Link>
+          <li className={`menu-item ${isActive('/productos')}`}>
+            <Link to="/productos" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={productoIcon} alt="Productos" />
+              </div>
+              <span className="menu-text">Productos</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={clienteIcon} alt="Icono de clientes" />
-            <Link to="/clientes" className="text">Clientes</Link>
+          <li className={`menu-item ${isActive('/clientes')}`}>
+            <Link to="/clientes" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={clienteIcon} alt="Clientes" />
+              </div>
+              <span className="menu-text">Clientes</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={pagosIcon} alt="Icono de pagos" />
-            <Link to="/pagos" className="text">Pagos</Link>
+          <li className={`menu-item ${isActive('/pagos')}`}>
+            <Link to="/pagos" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={pagosIcon} alt="Pagos" />
+              </div>
+              <span className="menu-text">Pagos</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={bolsasIcon} alt="Icono de bolsas de la suerte" />
-            <Link to="/bolsasSuerte" className="text">Bolsas de la suerte</Link>
+          <li className={`menu-item ${isActive('/bolsasSuerte')}`}>
+            <Link to="/bolsasSuerte" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={bolsasIcon} alt="Bolsas de la suerte" />
+              </div>
+              <span className="menu-text">Bolsas de la suerte</span>
+            </Link>
           </li>
-          <li className='apartado'>
-            <img src={videosIcon} alt="Icono de videos combos" />
-            <Link to="/videosCombos" className="text">Videos combos</Link>
+          <li className={`menu-item ${isActive('/videosCombos')}`}>
+            <Link to="/videosCombos" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={videosIcon} alt="Videos combos" />
+              </div>
+              <span className="menu-text">Videos combos</span>
+            </Link>
           </li>
-
-          <hr className='line'/>    
-
-          <li className='apartado'>
-            <img src={FinanzasIcon} alt="Icono de finanzas" />
-            <Link to="/finanzas" className="text">Finanzas</Link>
+          {/* Separador Estilizado */}
+          <div className="sidebar-divider"></div>
+          <li className={`menu-item ${isActive('/finanzas')}`}>
+            <Link to="/finanzas" className="menu-link">
+              <div className="icon-wrapper">
+                <img src={FinanzasIcon} alt="Finanzas" />
+              </div>
+              <span className="menu-text">Finanzas</span>
+            </Link>
           </li>
         </ul>
-      </div>
-
-
-
-
-
-      <div className='contenedor2'>
-        {/* Logo */}
-        <Link to="/perfil" className="noti">
-          {/* 2. Usas la variable entre llaves en el src */}
-          <img className='' src={notificacionIcon} alt="    Icon de notificacion" />
+      </aside>
+      {/* Barra de Usuario y Notificaciones (Top-Right) */}
+      <div className="header-actions">
+        {/* Botón de Notificaciones con Indicador de Alerta */}
+        <Link to="/notificacion" className="notification-btn" aria-label="Notificaciones">
+          <img src={notificacionIcon} alt="Notificaciones" />
+          <span className="notification-badge"></span> {/* Pulsing Badge */}
         </Link>
-        <Link to="/notificacion" className='perfil'>
-            <img src={PerfilIcon} alt="Foto del perfil" />
+        {/* Perfil del Usuario */}
+        <Link to="/perfil" className="profile-btn" aria-label="Ver Perfil">
+          <div className="profile-avatar-wrapper">
+            <img src={PerfilIcon} alt="Foto de perfil" className="profile-avatar-img" />
+          </div>
         </Link>
       </div>
     </nav>
   )
 }
-
-export default sideBar
+export default SideBar
